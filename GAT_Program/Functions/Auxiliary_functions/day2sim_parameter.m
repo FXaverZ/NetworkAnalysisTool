e16f18c2736249c1991f265c66cb6fddc34a23a2
Franz Zeilinger <franz.zeilinger@siemens.com>
2017-06-07 21:24:47 +0200

    Some minor changes, mainly extending comments in the functions and help texts

--- GAT_Program/Functions/Auxiliary_functions/day2sim_parameter.m
+++ GAT_Program/Functions/Auxiliary_functions/day2sim_parameter.m
@@ -1,19 +1,45 @@
 function [season, weekd, parafilemname] = day2sim_parameter(Model, act_day)
-%DAY2SIM_PARAMETER   calculates from a daynumber corresponding simulation parameters
-%   Detailed explanation goes here
+%DAY2SIM_PARAMETER   calculates out of a daynumber the corresponding simulation parameters 
+%   [SEASON, WEEKD, PARAFILEMNAME] = DAY2SIM_PARAMETER(MODEL, ACT_DAY) gets out of
+%   the actual daynumber ACT_DAY in form of a serial date number e.g. created by the
+%   DATNUM function, the corresponding simulation parametes:
+%		* Shortname of the current season SEASON
+%		* Shortname of the current day WEEKD
+%		* full filname to the parameterfilename for the load simulation PARAFILEMNAME
+%
+%	The MODEL structure is a description of the corresponding load simulation model,
+%	this sturcture contains at least 
+%		* two {3,1} Cell-Arrays with the used short names of the seasons and daytyps 
+%			MODEL.Seasons		
+%				1st row: shortname for summer season
+%				2nd row: shortname for the transition time
+%				3rd row: shortname for winter season
+%			MODEL.Weekdays
+%				1st row: shortname for working day typs (Monday till Friday)
+%				2nd row: shortname for saturday
+%				3rd row: shortname for sunday
+%		* one string entry, which indikates the seperation marker of filenameparts
+%		  for the used paramterfiles:
+%		MODEL.Seperator
+%
+%   The days are asigned to the seasson according to VDEW standard load profiles
+%   Summer: 15.5. - 14.9.   --> 123 days
+%   Winter: 1.11. - 20.3.   --> 140 days
+%   Transi: remaining days  --> 102 days
+%
+%	The shortnames SEASON and WEEKD can be mapped over HANDLES.SYSTEM.seasons and
+%	HANDLES.SYSTEM.weekdays to userfriendlier long names.
+%
+%   See also datenum, get_default_values_GAT
 
-% Version:                 0.0
-% Erstellt von:            Franz Zeilinger - 29.09.2015
-% Letzte Änderung durch:   
-
+% Version:                 1.0
+% Erstellt von:            Franz Zeilinger - 02.02.2015
+% Letzte Änderung durch:   Franz Zeilinger - 30.09.2015
 
 % das aktuelle Jahr ermitteln:
 Current_Year = str2double(datestr(act_day,'yyyy'));
 
-% Wie werden die Tage auf die Jahreszeiten aufgeteilt (nach VDEW):
-% Summer: 15.5. - 14.9.   --> 123 Tage
-% Winter: 1.11. - 20.3.   --> 140 Tage
-% Transi: sonst           --> 102 Tage
+% assign the day to the corresbonding season:
 sum_start = datenum(['15.05.',num2str(Current_Year)],'dd.mm.yyyy'); % Sommer-Beginn
 sum_end = datenum(['14.09.',num2str(Current_Year)],'dd.mm.yyyy');   % Sommer-Ende
 win_start = datenum(['01.11.',num2str(Current_Year)],'dd.mm.yyyy'); % Winter-Beginn
--- GAT_Program/Functions/get_default_values_GAT.m
+++ GAT_Program/Functions/get_default_values_GAT.m
@@ -1,5 +1,5 @@
 function handles = get_default_values_GAT(handles)
-%GET_DEFAULT_VALUES_GAT   loads the defaults of all settings in GAT
+%GET_DEFAULT_VALUES_GAT   loads the defaults of all settings in the GAT
 %   HANDLES = GET_DEFAULT_VALUES_NAT7(HANDLES) adds to the HANDLES
 %   Structure the default settings for the program behavior in form of the
 %   substructure 'Current_Settings'. This function has to be called right
@@ -7,9 +7,24 @@ function handles = get_default_values_GAT(handles)
 %
 %   See also GUIDATA.
 
-% Version:                 0.0
-% Erstellt von:            Franz Zeilinger - 29.09.2015
-% Letzte Änderung durch:   
+% Version:                 1.0
+% Created by:              Franz Zeilinger - 29.09.2015
+% Last change by:          Franz Zeilinger - 30.09.2015
 
+%------------------------------------------------------------------------------------
+% System-Values - are not to be altered during runtime!
+%------------------------------------------------------------------------------------
+
+% Standardbezeichnungen:
+System.seasons =   {... % Typen der Jahreszeiten
+	'Summer', 'Summer';... 
+	'Transi', 'Transition';...
+	'Winter', 'Winter';...
+	}; 
+System.weekdays =  {... % Typen der Wochentage
+	'Workda', 'Workday';...
+	'Saturd', 'Saturday';...
+	'Sunday', 'Sunday';...
+	}; 
 end
 
