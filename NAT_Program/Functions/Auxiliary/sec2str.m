f97667b36e4654ccb38519a499f51d52535a4bd8
z0033bda <z0033bda@host.docker.internal>
2021-11-22 14:34:44 +0100

    Enhanced sec2str to show higher time periods

--- NAT_Program/Functions/Auxiliary/sec2str.m
+++ NAT_Program/Functions/Auxiliary/sec2str.m
@@ -1,22 +1,36 @@
 function str = sec2str(t)
 %SEC2STR    ermittelt Zeitstring aus Zeitspanne
 %    STR = SEC2STR(T) ermittelt aus einer Zeitspanne T in Sekunden einen String der
-%    Form 'HHh MMmin SS.SSSsec' und gibt diesen zurück. Je nach Lände der
-%    Zeitspanne werden keine Angaben zu HH und MM gemacht bzw. die Genauigkeit
-%    der Zahlendarstellung angepasst.
+%    Form 'mm months dd days HHh MMmin SS.SSSs' und gibt diesen zurück. Je
+%    nach Lände der Zeitspanne werden keine Angaben zu HH und MM gemacht
+%    bzw. die Genauigkeit der Zahlendarstellung angepasst.
 
 % Erstellt von:            Franz Zeilinger - 11.08.2010
-% Letzte Änderung durch:   Franz Zeilinger - 27.02.2013
+% Letzte Änderung durch:   Franz Zeilinger - 22.11.2021
 
 sec_lin = datenum('1900-01-01 00:00:01')-datenum('1900-01-01 00:00:00');
 date = datenum('1900-01-01 00:00:00')+t*sec_lin;
-[~,~,~,h,min,sec] = datevec(date);
-if (h > 0)
+[~,m,d,h,min,sec] = datevec(date);
+m = m-1;
+d = d-1;
+if (m > 1)
+    str = [num2str(m),' months ',num2str(d),' days ',num2str(h),'h ',...
+		num2str(min),'min ',num2str(floor(sec)),'s'];
+elseif (m <= 1) && (m > 0)
+    str = [num2str(m),' month ',num2str(d),' day ',num2str(h),'h ',...
+		num2str(min),'min ',num2str(floor(sec)),'s'];
+elseif (d > 1)
+    str = [num2str(d),' days ',num2str(h),'h ',...
+		num2str(min),'min ',num2str(floor(sec)),'s'];
+elseif (d <= 1) && (d > 0)
+    str = [num2str(d),' day ',num2str(h),'h ',...
+		num2str(min),'min ',num2str(floor(sec)),'s'];
+elseif (d <= 0) && (h > 0)
 	str = [num2str(h),'h ',...
-		num2str(min),' min ',num2str(floor(sec)),' sec'];
+		num2str(min),'min ',num2str(floor(sec)),'s'];
 elseif (h <= 0) && (min > 0)
-	str = [num2str(min),' min ',num2str(floor(sec)),' sec'];
+	str = [num2str(min),'min ',num2str(floor(sec)),'s'];
 else
-	str = [num2str(sec,'%3.1f'),' sec'];
+	str = [num2str(sec,'%3.1f'),'s'];
 end
 		
