34e3e2d297d0594ec98e8777ffeec9df2a288486
Franz Zeilinger <franz.zeilinger@siemens.com>
2022-02-12 16:16:10 +0100

    Added more infos during OAT data export

--- OAT_Program/Auxiliary_functions/save_NVIEW_data.m
+++ OAT_Program/Auxiliary_functions/save_NVIEW_data.m
@@ -2,15 +2,20 @@ function save_NVIEW_data(hObject,handles)
 %SAVE_NVIEW_DATA Summary of this function goes here
 %   Detailed explanation goes here
 
+sep = ' - ';
+titlestr = 'Saving OAT Data File...';
 % Set default location for UI get file to open at (handles >> System)
 handles.NVIEW_Control.OAT_Data_File.Path = handles.System.Main_Path;
+sourcefile = handles.NVIEW_Control.Result_Information_File;
+result_Name = strsplit(sourcefile.Name,sep);
+result_Name = result_Name{1};
 
 % Select OAT result file
 file = handles.NVIEW_Control.OAT_Data_File;
 % Open UI window and select result information file
 file.Path = uigetdir(...
     [file.Path,filesep],...
-	'Location for OAT data file...');
+	titlestr);
 % Check whether an invalid location has been specified:
 if isequal(file.Path,0)  
     % If file is invalid, exit this function and update display of main window    
@@ -21,6 +26,60 @@ if isequal(file.Path,0)
 end
 
 handles.NVIEW_Control.OAT_Data_File = file;
+
+% load the OAT data of the file...
+handles = update_NVIEW_control_panel(handles, 'Saving OAT Data, please wait...\n', 'clear');
+
+filename_add = [sep,'000'];
+
+% make a subfolder
+if ~isfolder([file.Path,filesep,result_Name,filename_add])
+    mkdir([file.Path,filesep,result_Name,filename_add])
+else
+    selection = questdlg({...
+        'A result exoprt for result id';...
+        ['    "',result_Name,'"'];...
+        'alread exixts. How should be further progessed?';...
+        '';...
+        '"Overwrite" = overwrite the existing files';...
+        '"Keep" = create a new version with with a suffix " - XXX"';...
+        },titlestr,'Overwrite','Keep','Cancel','Keep');
+    switch selection
+        case 'Overwrite'
+            delete([file.Path,filesep,result_Name,filename_add,filesep,'*']);
+        case 'Keep'
+            new_filename_add_found  = false;
+            new_filename_add_number = 1;
+            while ~ new_filename_add_found
+                filename_add = [sep,num2str(new_filename_add_number,'%03d')];
+                if ~isfolder([file.Path,filesep,result_Name,filename_add])
+                    mkdir([file.Path,filesep,result_Name,filename_add])
+                    new_filename_add_found = true;
+                end
+                new_filename_add_number = new_filename_add_number + 1;
+            end
+    end
+end
+% copy the settings file and the corresponding log-file in the new
+% directory for later reference:
+copyfile([sourcefile.Path,filesep,sourcefile.Name,sourcefile.Exte],[file.Path,filesep,result_Name,filename_add]);
+copyfile([sourcefile.Path,filesep,result_Name,sep,'Log.log'],[file.Path,filesep,result_Name,filename_add]);
+
+[~, infostr] = update_NVIEW_control_panel_simulation_options(handles);
+infostr = sprintf([...
+    'Content of this data file:\n',...
+    '--------------------------\n',...
+    strrep(infostr,'%','%%'),...
+    '--------------------------\n',...
+    ]);
+[~, infostr] = update_NVIEW_control_panel_analysis_selection(handles,infostr);
+infostr = sprintf([...
+    strrep(infostr,'%','%%'),'\n',...
+    '--------------------------',...
+    ]);
+fid = fopen([file.Path,filesep,result_Name,filename_add,filesep,result_Name,sep,'OAT-Data-Info.txt'],'w');
+fprintf(fid, strrep(infostr,'%','%%'));
+fclose(fid);
 
 handles = process_nat_results_as(handles); % Process results
 
@@ -42,16 +101,11 @@ if isfield(handles, 'NVIEW_Results')
     NVIEW_Results = handles.NVIEW_Results;
 end
 
-% load the OAT data of the file...
-handles = update_NVIEW_control_panel(handles, 'Saving OAT Data, please wait...\n', 'clear');
-
-
-simdate = datestr(now,'yyyy-mm-dd_HH-MM-SS');
-save([file.Path,filesep,'Res_',simdate,' - OAT-Data'],variables{:},'-v7.3')
+save([file.Path,filesep,result_Name,filename_add,sep,'OAT-Data','.mat'],variables{:},'-v7.3')
 
 % Inform the user:
 str = 'Data successfully saved!';
-helpdlg(str, 'Saving OAT Data File...');
+helpdlg(str, titlestr);
 
 handles = refresh_display_NVIEW_main_gui(handles);
 
