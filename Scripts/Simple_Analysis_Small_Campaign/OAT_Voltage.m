576e4ff85f9e4b4205cf88e80b5f99d38ec45ad7
Franz Zeilinger <groszpuerstinger@gmail.com>
2022-03-05 11:17:58 +0100

    Finished first voltage summary

--- Scripts/Simple_Analysis_Small_Campaign/OAT_Voltage.m
+++ Scripts/Simple_Analysis_Small_Campaign/OAT_Voltage.m
@@ -43,31 +43,33 @@ for i = 1: numel(folders)
 end
 disp('... done!');
 Saved_Data_OAT.Number_Datasets = numel(folders);
-clear scriptpath sep folders i NVIEW_*
+clear script* sep folders i NVIEW_*
 
 % = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 %% Additional Set Up / Configuration
 % = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 Settings_Scenario = {...
-% 1     2                                                3             4          5
-% ID  , Filename                                       , Color       , LineStyle, String for legend 
-	 1,'01_SB_Base_Winter_Workda'                      ,[ 74,126,187],'-','Base Winter';...
-	 2,'02_SB_Base_Summer_Workda'                      ,[ 74,126,187],'-','Base Summer';...
-	 3,'03_S1_LowLoadHighInfeed_Winter_Workda'         ,[190, 75, 72],'-','Low Load High Infeed Winter';...
-	 4,'04_S1_LowLoadHighInfeed_Summer_Workda'         ,[190, 75, 72],'-','Low Load High Infeed Summer';...
-	 5,'05_S2_HighLoadHighInfeed_Winter_Workda'        ,[152,185, 84],'-','High Load High Infeed Winter';...
-	 6,'06_S2_HighLoadHighInfeed_Summer_Workda'        ,[152,185, 84],'-','High Load High Infeed Summer';...
-	 7,'07_S3_HighLoadHighInfeed2Nodes_Winter_Workda'  ,[128,100,162],'-','High Load High Infeed (2 Nodes) Winter';...
-	 8,'08_S3_HighLoadHighInfeed2Nodes_Summer_Workda'  ,[128,100,162],'-','High Load High Infeed (2 Nodes) Summer';...
-	 9,'09_S4_MediumLoadHighInfeed2Nodes_Winter_Workda',[247,173, 36],'-','Medium Load High Infeed (2 Nodes) Winter';...
-	10,'10_S4_MediumLoadHighInfeed2Nodes_Summer_Workda',[247,173, 36],'-','Medium Load High Infeed (2 Nodes) Summer';...
+% 1     2                                                 3                 4          5
+% ID ,  Filename                                        , Color           , LineStyle, String for legend 
+	 1, '01_SB_Base_Winter_Workda'                      ,[ 74,126,187]/256, '-'      , 'SB Winter';...
+	 2, '02_SB_Base_Summer_Workda'                      ,[ 74,126,187]/256, '-'      , 'SB Summer';...
+	 3, '03_S1_LowLoadHighInfeed_Winter_Workda'         ,[190, 75, 72]/256, '-'      , 'S1 Winter';...
+	 4, '04_S1_LowLoadHighInfeed_Summer_Workda'         ,[190, 75, 72]/256, '-'      , 'S1 Summer';...
+	 5, '05_S2_HighLoadHighInfeed_Winter_Workda'        ,[152,185, 84]/256, '-'      , 'S2 Winter';...
+	 6, '06_S2_HighLoadHighInfeed_Summer_Workda'        ,[152,185, 84]/256, '-'      , 'S2 Summer';...
+	 7, '07_S3_HighLoadHighInfeed2Nodes_Winter_Workda'  ,[128,100,162]/256, '-'      , 'S3 Winter';...
+	 8, '08_S3_HighLoadHighInfeed2Nodes_Summer_Workda'  ,[128,100,162]/256, '-'      , 'S3 Summer';...
+	 9, '09_S4_MediumLoadHighInfeed2Nodes_Winter_Workda',[247,173, 36]/256, '-'      , 'S4 Winter';...
+	10, '10_S4_MediumLoadHighInfeed2Nodes_Summer_Workda',[247,173, 36]/256, '-'      , 'S4 Summer';...
 	};
 
 Settings_GridVariants = {...
-    'g01_Base_NS_50_Nodes',             'Basisnetz',               '-';...
-    'g02_Repalce_OH_Lines_With_Cables', 'Ersatz Oberleitung',      '--';...
-    'g03_Add_Cable_to_First_OH_Line',   'Verstärkung Oberleitung', ':';...
-    'g04_Add_Cable_to_Weak_Cables',     'Verstärkung Kabel',       '-.';...
+% 1     2                                  3                 4          5
+% ID ,  Sub-Structure Name               , Color           , LineStyle, String for legend 
+    1, 'g01_Base_NS_50_Nodes'            ,[256,256,256]/256, '--'     , 'Basisnetz';...
+    2, 'g02_Repalce_OH_Lines_With_Cables',[256,256,256]/256, '-.'     , 'Ersatz Oberleitung';...
+    3, 'g03_Add_Cable_to_First_OH_Line'  ,[256,256,256]/256, ':'      , 'Verstärkung Oberleitung';...
+    4, 'g04_Add_Cable_to_Weak_Cables'    ,[256,256,256]/256, '-'      , 'Verstärkung Kabel';...
 	};
 
 Settings_Datasets = {
@@ -78,31 +80,27 @@ Settings_Datasets = {
 
 Settings_Number_Profiles = 10;
 % = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-%% Plot the distribution of voltage band violations per grid variant
+%% Plot the Mean Voltage Values
 % = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-Option_Active_Scenarios = 2:2:10;   % Sommer
-% Option_Active_Scenarios = [6, 8];
+Option_Active_Scenarios = [6, 8];
+% Option_Active_Scenarios = 2:2:10;   % Sommer
+% Option_Active_Scenarios = 1:2:10;   % Winter
+% Option_Active_Scenarios = 1:10;     % All scenarios (not recomended!)
 % Option_Active_Scenarios = 8;
-% Option_Active_Scenarios = 1:2:10; % Winter
-% Option_Active_Scenarios = 1:10;   % All scenarios (not recomended!)
 %- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-Option_Active_GridVariants = [2,3,4];     % 
+Option_Active_GridVariants = 1:4;     % all grid varaiants
 % Option_Active_GridVariants = 2;
 %- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-% y-axis Settings (Option_y_max_Value = -1 ... autoscale)
-Option_y_max_Value  = 1.05; % -1 ... autoscale
-Option_y_min_Value  = 0.95;
+% Y-axis Settings
+Option_y_max_Value  = -1;   % -1 ... autoscale
+Option_y_min_Value  = 0.97;
 Option_y_step_Value = 0.02; % -1 ... autostep
 % = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-if numel(Option_Active_GridVariants) < 2
-	Labels_Title = ['Mittlerer Verlauf Spannung für Netzvariante "',Settings_GridVariants{Option_Active_GridVariants,2},'"'];
-else
-	if numel(Option_Active_Scenarios) > 1
-		Labels_Title = 'Mittlerer Verlauf Spannung';
-	else
-		Labels_Title = ['Mittlerer Verlauf Spannung für Scenario "',Settings_Scenario{Option_Active_Scenarios,2},'"'];
-	end
-end
+% Label and title strings:
+Labels_Title_full_Comparison =  'Mittlerer Verlauf Spannung'; % Title, if > 1 scenario and > 1 grid variant...
+Labels_Title_one_Variant     = ['Mittlerer Verlauf Spannung für Netzvariante "',Settings_GridVariants{Option_Active_GridVariants,2},'"'];
+Labels_Title_one_Scenario    = ['Mittlerer Verlauf Spannung für Scenario "',Settings_Scenario{Option_Active_Scenarios,2},'"'];
+Option_show_Title  = 1; % 1 ... show Title, 0 ... no Title for export to Word...
 Labels_X_Direction = 'Tageszeit [h]';
 Labels_Y_Direction = 'Spannung [p.u.]';
 % = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
@@ -112,43 +110,66 @@ for i = 1 : Saved_Data_OAT.Number_Datasets
 		Active_Scenarios = Settings_Scenario(Option_Active_Scenarios,:);
 		Active_GridVars  = Settings_GridVariants(Option_Active_GridVariants,:);
 		
+		if Option_show_Title
+			if numel(Option_Active_GridVariants) < 2
+				Labels_Title = Labels_Title_one_Variant;
+			else
+				if numel(Option_Active_Scenarios) > 1
+					Labels_Title = Labels_Title_full_Comparison;
+				else
+					Labels_Title = Labels_Title_one_Scenario;
+				end
+			end
+		else
+			Labels_Title = []; %#ok<UNRCH>
+		end
+		
 		fig_oat_voltage_sum = set_up_tiledlayout(Labels_Title, Labels_X_Direction, Labels_Y_Direction);
 		
 	end
 	
 	figure(fig_oat_voltage_sum); nexttile;
-	Labels_Scenarios = {};
-	Labels_Grid      = {};
-	Labels_Gridstyle = [];
-	d = Saved_Data_OAT.(['Saved_',num2str(i)]).NVIEW_Processed;
+	Labels_Scenarios  = {};
+	Labels_Scen_Style = [];
+	Labels_Grid       = {};
+	Labels_Grid_Style = [];
+	Data = Saved_Data_OAT.(['Saved_',num2str(i)]).NVIEW_Processed;
 	
 	for j = 1:size(Active_GridVars,1)
 		for k = 1 : size(Active_Scenarios,1)
-			voltage_timeline =...
-				zeros(1,d.Control.Simulation_Options.Timepoints_per_dataset);
-			for t = 1 : d.Control.Simulation_Options.Timepoints_per_dataset
-				voltage_timeline(t) = nansum(nansum(squeeze(nansum(squeeze(d.(Active_GridVars{j,1}).bus_voltages(Active_Scenarios{k,1},:,t,:,:)))))) / ...
-					(size(d.(Active_GridVars{j,1}).bus,1)*d.Control.Simulation_Options.Number_of_datasets*3);
+			Data_Voltage_Timeline =...
+				zeros(1,Data.Control.Simulation_Options.Timepoints_per_dataset);
+			for t = 1 : Data.Control.Simulation_Options.Timepoints_per_dataset
+				Data_Voltage_Timeline(t) = nansum(nansum(squeeze(nansum(squeeze(Data.(Active_GridVars{j,2}).bus_voltages(Active_Scenarios{k,1},:,t,:,:)))))) / ...
+					(size(Data.(Active_GridVars{j,2}).bus,1)*Data.Control.Simulation_Options.Number_of_datasets*3);
 			end
-			figure(fig_oat_voltage_sum); l = plot(voltage_timeline);
-			set(l, 'Color', Active_Scenarios{k,3}/256);
-			set(l, 'LineStyle', Active_Scenarios{k,4});
+			figure(fig_oat_voltage_sum); pl = plot(Data_Voltage_Timeline);
+			set(pl, 'Color', Active_Scenarios{k,3});
+			set(pl, 'LineStyle', Active_Scenarios{k,4});
 			if size(Active_GridVars,1) > 1
-				set(l, 'LineStyle', Active_GridVars{j,3});
+				set(pl, 'LineStyle', Active_GridVars{j,4});
 			end
 			drawnow;
 			if j == 1
+				% get the legend entries for the scenarios:
 				Labels_Scenarios{end+1} = Active_Scenarios{k,5}; %#ok<SAGROW>
+				pl = plot(nan, nan);	                         % make an invisible line for legend	
+				set(pl,...
+					'Color', Active_Scenarios{k,3},...       % set color of invisible line
+					'LineStyle', Active_Scenarios{k,4});         % set linestyle of invisible line
+				Labels_Scen_Style(end+1) = pl; %#ok<SAGROW>
 			end
 			if k <=1
 				figure(fig_oat_voltage_sum); hold on;
 			end
 		end
-		l = plot(nan, nan);
-		set(l, 'Color', 'k');
-		set(l, 'LineStyle', Active_GridVars{j,3});
-		Labels_Gridstyle(end+1) = l; %#ok<SAGROW>
-		Labels_Grid{end+1} = Active_GridVars{j,2}; %#ok<SAGROW>
+		% get the legend entries for the grid variants:
+		Labels_Grid{end+1} = Active_GridVars{j,5}; %#ok<SAGROW>
+		pl = plot(nan, nan);	                   % make an invisible line for legend
+		set(pl,...
+			'Color', 'k',...                       % set color of invisible line
+			'LineStyle', Active_GridVars{j,4});    % set linestyle of invisible line
+		Labels_Grid_Style(end+1) = pl; %#ok<SAGROW>
 	end
 	% Format Diagrams:
 	figure(fig_oat_voltage_sum); 
@@ -156,10 +177,10 @@ for i = 1 : Saved_Data_OAT.Number_Datasets
 	ax.Title.String = ['Profilsatz ',num2str(i)];
 	% Legend
 	if i == 1 && numel(Option_Active_Scenarios) > 1
-		legend(Labels_Scenarios);
+		legend(Labels_Scen_Style, Labels_Scenarios);
 	end
 	if i == 2 && numel(Option_Active_GridVariants) > 1
-		l = legend(Labels_Gridstyle, Labels_Grid);
+		legend(Labels_Grid_Style, Labels_Grid);
 	end
 	% X Axis
 	[tick_x_Positions, tick_x_Labels] = get_tick_x_single_day_profile();
@@ -175,10 +196,10 @@ for i = 1 : Saved_Data_OAT.Number_Datasets
 			ax.YAxis.TickValues   = tick_y_Positions;
 			ax.YAxis.TickLabels   = tick_y_Labels;
 		end
-
 	end
 	set_default_plot_properties(ax);
 	figure(fig_oat_voltage_sum); hold off;
 end
 
+clear Active_* ax Data i j k t Labels_* Option_* pl tick_*
 % = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
