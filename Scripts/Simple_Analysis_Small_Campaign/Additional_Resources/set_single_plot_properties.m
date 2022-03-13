b34a148d76064c37d6c13f4aee06e19b811515cd
Franz Zeilinger <groszpuerstinger@gmail.com>
2022-03-13 13:37:44 +0100

    Clean up of function for further enhancement

--- Scripts/Simple_Analysis_Small_Campaign/Additional_Resources/set_single_plot_properties.m
+++ Scripts/Simple_Analysis_Small_Campaign/Additional_Resources/set_single_plot_properties.m
@@ -1,25 +1,26 @@
-function set_single_plot_properties(ax, titlestr, xlabestr, ylabelstr, show_title)
+function set_single_plot_properties(ax, titlestr, xlabelstr, ylabelstr, show_title, varargin)
 %SET_SINGLE_PLOT_PROPERTIES Summary of this function goes here
 %   Detailed explanation goes here
 
-if ~isempty(xlabestr)
-	ax.XLabel.String = xlabestr;
+if ~isempty(xlabelstr)
+	ax.XLabel.String = xlabelstr;
+	ax.XLabel.FontWeight = 'bold';
 end
 if ~isempty(ylabelstr)
 	ax.YLabel.String = ylabelstr;
+	ax.YLabel.FontWeight = 'bold';
+end
+if show_title && ~isempty(titlestr)
+	ax.Title.String = titlestr;
 end
 
-if show_title
-	if ~isempty(titlestr)
-		ax.Title.String = titlestr;
-	end
-	ax.Position = [0.06, 0.11, 0.92, 0.83];
+if show_title && ~isempty(titlestr)
+	Position = [0.06, 0.11, 0.92, 0.83];
 else
-	ax.Position = [0.06, 0.11, 0.92, 0.86];
+	Position = [0.06, 0.11, 0.92, 0.86];
 end
 
-ax.XLabel.FontWeight = 'bold';
-ax.YLabel.FontWeight = 'bold';
+ax.Position = Position;
 
 end
 
