151a6aa46f3cffdb3cc811fe869c089adef51fec
Franz Zeilinger <groszpuerstinger@gmail.com>
2022-03-18 18:23:53 +0100

    FIX: RMS now in percent as intended

--- Scripts/Simple_Analysis_Small_Campaign/Additional_Resources/calculate_rms_error.m
+++ Scripts/Simple_Analysis_Small_Campaign/Additional_Resources/calculate_rms_error.m
@@ -2,6 +2,6 @@ function Data_Boundaries = calculate_rms_error(Data)
 Data_Diff = Data(:,2:end,:) - Data(:,1:end-1,:);
 	Data_Diff = sqrt(Data_Diff .* Data_Diff);
 	Data_Diff = squeeze(sum(Data_Diff))./squeeze(sum(Data(:,1:end-1,:)));
-	Data_Boundaries = [min(Data_Diff,[],2), max(Data_Diff,[],2)];
+	Data_Boundaries = 100*[min(Data_Diff,[],2), max(Data_Diff,[],2), mean(Data_Diff,2)];
 end
 
